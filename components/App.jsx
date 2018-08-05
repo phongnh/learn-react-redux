b47887a1fd1c1a9beaa230b21259502d3d569a23
Phong Nguyen <nhphong1406@gmail.com>
2018-08-06 00:56:15 +0700

    Fixes lint command

--- components/App.jsx
+++ components/App.jsx
@@ -1,9 +1,7 @@
-//This will be inside components
+// This will be inside components
 
-import React, { Component } from 'react';
+import React from 'react';
 
-const App = () => {
-    return <h1>Hello World!</h1>
-}
+const App = () => <h1>Hello World!</h1>;
 
-export default App
+export default App;
